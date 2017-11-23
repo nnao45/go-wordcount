f9bd5c9cb0f48e95f7551893678ad6dbf41e0450
nnao45 <n4sekai5y@gmail.com>
2017-11-23 16:58:04 +0900

    Update wordcount.go

--- wordcount.go
+++ wordcount.go
@@ -1,6 +1,6 @@
 package wordcount
 
-func WC(runes string, word string) (i int){
+func WC(word string, runes string) (i int){
         rs := []rune(runes)
         var k int
         for _, r := range word {
