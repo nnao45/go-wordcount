11a850cc4a34cb4cec374ed49f1441ad4fc7c9a0
nnao45 <n4sekai5y@gmail.com>
2017-11-23 16:34:45 +0900

    Create main.go

---
+++ cmd/main.go
@@ -0,0 +1,17 @@
+package wordcount
+
+func WC(runes []rune, word string) (i int) {
+        var k int
+        for _, r := range word {
+                if r == runes[k] {
+                        if k < len(runes)-1 {
+                                k++
+                        } else if k == len(runes)-1 {
+                                i++
+                        }
+                } else {
+                        k = 0
+                }
+        }
+        return
+}
